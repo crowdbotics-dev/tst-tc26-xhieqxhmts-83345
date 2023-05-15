996e04f5ff2a4589db1b62764d7963355b176353
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 09:41:33 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,155 +1,150 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
-  Text,
   View,
-  StyleSheet,
+  ImageBackground,
   Image,
-  Dimensions,
-  Pressable
+  Text,
+  TouchableOpacity,
+  ScrollView
 } from "react-native";
-import { getItem } from "../../store";
+import {
+  NavigationHelpersContext,
+  useNavigationBuilder,
+  TabRouter,
+  TabActions,
+  createNavigatorFactory
+} from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { createStackNavigator } from "@react-navigation/stack";
+import { BACKGROUND_URL, LOGO_URL } from "./screens/constants.js";
+import { slice } from "./auth";
+import { styles } from "./screens/styles";
+import { SignInTab, SignupTab } from "./screens/loginsignup";
+import PasswordReset from "./screens/reset";
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
-
-const Welcome = ({ navigation }) => {
-  const checkAuth = async () => {
-    const token = await getItem("token")
-    if (token) {
-      navigation.replace('homeScreen')
-    }
-  }
-
-
-
-  useEffect(() => {
-    checkAuth();
-  }, [])
+const LoginTabBar = ({ navigation, state, descriptors }) => {
+  const currentTab = state.routes[state.index];
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.background}
-        // @ts-ignore
-        source={require("./assets/welcome.png")}
+    <View style={styles.tabStyle}>
+      {state.routes.map((route) => (
+        <View
+          key={route.key}
+          style={route.key === currentTab.key ? styles.activeTabStyle : null}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              const event = navigation.emit({
+                type: "tabPress",
+                target: route.key,
+                canPreventDefault: true
+              });
+              if (!event.defaultPrevented) {
+                navigation.dispatch({
+                  ...TabActions.jumpTo(route.name),
+                  target: state.key
+                });
+              }
+            }}
+          >
+            <Text style={styles.tabStyle}>
+              {descriptors[route.key].options.title || route.name}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+function LoginSignupTabs({ initialRouteName, children, screenOptions }) {
+  const { state, navigation, descriptors } = useNavigationBuilder(TabRouter, {
+    children,
+    screenOptions,
+    initialRouteName
+  });
+
+  return (
+    <NavigationHelpersContext.Provider value={navigation}>
+      <KeyboardAwareScrollView contentContainerStyle={{ flex: 1 }}>
+        <ScrollView style={[styles.container]}>
+          <View style={{ flex: 1 }}>
+            <View style={styles.imageContainer}>
+              <ImageBackground
+                source={{
+                  uri: BACKGROUND_URL
+                }}
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  resizeMode: "cover",
+                  height: "100%",
+                  width: "100%"
+                }}
+              >
+                <Image
+                  source={{
+                    uri: LOGO_URL
+                  }}
+                  style={{
+                    width: 155,
+                    height: 155,
+                    alignSelf: "center",
+                    resizeMode: "contain"
+                  }}
+                />
+              </ImageBackground>
+            </View>
+          </View>
+          <View style={[styles.cardView]}>
+            <LoginTabBar
+              navigation={navigation}
+              state={state}
+              descriptors={descriptors}
+            />
+            <View style={styles.tabContainerStyle}>
+              {descriptors[state.routes[state.index].key].render()}
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+    </NavigationHelpersContext.Provider>
+  );
+}
+
+const createLoginNavigator = createNavigatorFactory(LoginSignupTabs);
+
+const LoginStack = createLoginNavigator();
+
+const LoginScreen = () => {
+  return (
+    <LoginStack.Navigator>
+      <LoginStack.Screen
+        name="SignIn"
+        component={SignInTab}
+        options={{ title: "Sign In" }}
       />
-      <Text style={styles.heading}>Welcome Back</Text>
-      <Text style={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non at sed.
-      </Text>
-      <View style={styles.buttonContainer}>
-        <Button buttonText="Sign Up" style={styles.button} onPress={() => navigation.navigate("login", {route: "SignUpScreen"})} />
-        <Button
-          buttonText="Login"
-          style={styles.button}
-          borderColor="#000"
-          backgroundColor="#fff"
-          textColor="#000"
-          hideShadow
-          onPress={() => navigation.navigate("login", {route: "LoginScreen"})}
-        />
-      </View>
-    </View>
+      <LoginStack.Screen
+        name="SignUp"
+        component={SignupTab}
+        options={{ title: "Sign Up" }}
+      />
+    </LoginStack.Navigator>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    width: windowWidth,
-    height: windowHeight
-  },
-  heading: {
-    marginTop: "20%",
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#000",
-    textAlign: "center"
-  },
-  description: {
-    fontSize: 14,
-    color: "#888888",
-    textAlign: "center",
-    paddingHorizontal: 40,
-    marginVertical: 20
-  },
-  buttonContainer: {
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    marginTop: 20
-  },
-  button: {
-    marginHorizontal: 20,
-    marginVertical: 10
-  },
-  background: {
-    width: 400,
-    height: 350,
-    resizeMode: "stretch"
-  }
-});
 
-export default Welcome;
+const Stack = createStackNavigator();
 
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "#000";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
-  };
+const LoginSignup = () => {
   return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={buttonStyles.childrenContainer}>{params.children}</View>
-        </Pressable>
-      </View>
-    </View>
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="PasswordReset" component={PasswordReset} />
+    </Stack.Navigator>
   );
 };
 
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
-  },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
-  },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-
-    flexDirection: "row"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
+export default {
+  title: "login",
+  navigator: LoginSignup,
+  slice: slice
+};
